80a6b8afd2ef7da8ec799cd655e2e7218e039cd2
Elliot Wolk <elliot.wolk@gmail.com>
2012-01-20 05:14:37 -0500

    apt-get update first

--- install-packages.pl
+++ install-packages.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 
+system 'n9', '-s', 'apt-get', 'update';
+
 my @packages = qw( bash python parted );
 for my $pkg(@packages){
   system 'n9', '-s', 'apt-get', 'install', $pkg;
