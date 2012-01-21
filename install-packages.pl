10c0d0b9c8dc8d57399e2f53c3ed12a3929402f3
Elliot Wolk <elliot.wolk@gmail.com>
2012-01-21 04:06:10 -0500

    added packages for building, and added mplayer install from deb

--- install-packages.pl
+++ install-packages.pl
@@ -4,7 +4,23 @@ use warnings;
 
 system 'n9', '-s', 'apt-get', 'update';
 
-my @packages = qw( bash bash-completion python parted );
+my @packages = qw(
+  bash bash-completion
+  parted git
+  python perl
+  kernel-source linux-kernel-headers
+  gcc make libc6-dev libc-dev bzip2);
 for my $pkg(@packages){
   system 'n9', '-s', 'apt-get', 'install', $pkg;
+}
+
+
+my @debs = `ls packages/`;
+
+print "\n\nCopying and installing these debs:\n---\n@debs---\n";
+my $dir = '/opt/manual-packages';
+system "rsync packages/ root@`n9`:$dir -av --progress --delete";
+for my $deb(@debs){
+  chomp $deb;
+  system 'n9', '-s', 'dpkg', '-i', "$dir/$deb"; 
 }
