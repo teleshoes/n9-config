f89c6944af7309d52c65fbf6ae96f568ab79fd6e
Elliot Wolk <elliot.wolk@gmail.com>
2012-03-19 13:05:34 -0400

    add imagemagick package

--- install-packages.pl
+++ install-packages.pl
@@ -18,6 +18,7 @@ my %pkgGroups = (
     perl bash-completion python python-apt
     mcetools bzip2 sqlite3
     x11-utils xresponse
+    imagemagick
     python-pyside.qtgui python-qmsystem python-pyside.qtdeclarative
   )],
   '3' => [qw(
