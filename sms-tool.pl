f39e6144462d08f0b08198b67db16e164a37bc1b
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-12 11:24:14 -0400

    added backup to run on the phone, git init when no git repo exists, and mkdir the backup and repo dirs

--- sms-tool.pl
+++ sms-tool.pl
@@ -1,4 +1,19 @@
 #!/usr/bin/perl
+#n9-sms-tool v0.1
+#Copyright 2012 Elliot Wolk
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.
+#If not, see <http://www.gnu.org/licenses/>.
 use strict;
 use warnings;
 
@@ -19,12 +34,15 @@ sub removeDupes(\@);
 sub main(@){
   my $arg = shift;
   $arg = '' if not defined $arg;
-  my @okArgs = qw(split join commit);
+  my @okArgs = qw(split join commit backup);
   my $ok = join "|", @okArgs;
   die "Usage: $0 $ok\n" if $arg !~ /^($ok)$/ or @_ > 0;
 
   if($arg eq 'split'){
-    my @messages = (getMessagesFromDir($smsDir), getMessagesFromDir($repoDir));
+    system "mkdir $repoDir -p";
+    my @messages = (
+      getMessagesFromDir($smsDir),
+      getMessagesFromDir($repoDir));
     @messages = removeDupes @messages;
     system "mkdir -p $repoDir";
     system "rm $repoDir/*.sms";
@@ -36,9 +54,16 @@ sub main(@){
     writeMessageFile @messages, "$smsDir/filtered.sms";
   }elsif($arg eq 'commit'){
     chdir $repoDir;
+    if(not -d '.git'){
+      system "git init";
+    }
     system "git add *.sms";
     system "git --no-pager diff --cached";
     system "git commit -m 'automatic commit'";
+  }elsif($arg eq 'backup'){
+    system "mkdir $smsDir -p";
+    chdir $smsDir;
+    system "smsbackuprestore", "export", time . ".sms";
   }
 }
 
