670430299828bfa3be4b1fc7b84111d2950b9c3f
Elliot Wolk <elliot.wolk@gmail.com>
2012-09-04 13:41:08 -0400

    added trigger system to config-overwrite

--- CONFIG_FILES/config-overwrite.pl
+++ CONFIG_FILES/config-overwrite.pl
@@ -18,8 +18,12 @@ my %symlinksToReplace = map {$_ => 1} (
   "$bgDir/meegotouch-desktop-bg.jpg",
 );
 
+my %changedTriggers = (
+);
+
 sub overwriteFile($$);
 sub removeFile($);
+sub md5sum($);
 
 sub main(@){
   die "Usage: $0\n" if @_ > 0;
@@ -30,11 +34,24 @@ sub main(@){
   my @filesToRemove = `cat $DIR/config-files-to-remove`;
   chomp foreach @filesToRemove;
 
+  my %triggers;
+
   print "\n ---handling boing files...\n";
   for my $file(@boingFiles){
     my $dest = $file;
     $dest =~ s/%/\//g;
+    my ($old, $new);
+    if(defined $changedTriggers{$dest}){
+      $old = md5sum $dest;
+    }
     overwriteFile "$DIR/$file", $dest;
+    if(defined $changedTriggers{$dest}){
+      $new = md5sum $dest;
+      if($old ne $new){
+        print "   ADDED TRIGGER: $changedTriggers{$dest}\n";
+        $triggers{$changedTriggers{$dest}} = 1;
+      }
+    }
   }
 
   print "\n ---handling bin files...\n";
@@ -46,6 +63,12 @@ sub main(@){
   for my $file(@filesToRemove){
     chomp $file;
     removeFile $file;
+  }
+
+  print "\n ---running triggers...\n";
+  for my $trigger(keys %triggers){
+    print "  $trigger: \n";
+    system $trigger;
   }
 }
 
@@ -90,4 +113,15 @@ sub removeFile($){
   }
 }
 
+sub md5sum($){
+  my $file = shift;
+  my $out;
+  if(-d $file){
+    $out = `md5sum "$file"/* 2>/dev/null | sort`;
+  }else{
+    $out = `md5sum $file 2>/dev/null`;
+    chomp $out;
+  }
+  return $out;
+}
 &main(@ARGV);
