e15ee58feecde8b62ae8fbadad67f175a738ca0d
Elliot Wolk <elliot.wolk@gmail.com>
2013-11-13 20:26:36 -0500

    Revert "packages: add facebook back"
    This reverts commit 4e05b2791ae424a14a7a0f944fc65044b1700c9a.

--- packages.pl
+++ packages.pl
@@ -18,6 +18,7 @@ my @packagesToRemove = qw(
   ovi-music-store morpheus morpheus-guard
 
   mp-harmattan-001-pr
+  facebook facebookqml libqt-facebook facebook-meego
   twitter twitter-qml libmeegotwitter1 twitter-meego
 );
 
