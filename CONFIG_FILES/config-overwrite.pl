d11e375242c811e82f2395c43521a6dd054e00dc
Elliot Wolk <elliot.wolk@gmail.com>
2014-06-01 13:00:23 -0400

    config-overwrite: add a trigger for bluetooth

--- CONFIG_FILES/config-overwrite.pl
+++ CONFIG_FILES/config-overwrite.pl
@@ -25,6 +25,7 @@ my %changedTriggers = (
   "$bgDir/meegotouch-desktop-bg.jpg" => 'reload-wallpaper',
   "/home/user/.config/ProfileMatic/rules.conf" =>
     "initctl restart apps/profilematicd",
+  "/var/lib/bluetooth" => "initctl restart xsession/bluetoothd",
 );
 
 sub overwriteFile($$);
