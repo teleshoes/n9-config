1d129525c3384ec2074ea0e273c3ee6aa678d913
Elliot Wolk <elliot.wolk@gmail.com>
2015-07-23 23:05:27 -0400

    config-overwrite: push --del into overwriteFile() as an option

--- CONFIG_FILES/config-overwrite.pl
+++ CONFIG_FILES/config-overwrite.pl
@@ -11,7 +11,6 @@ my $binTarget = '/usr/bin';
 
 my @rsyncOpts = qw(
   -a  --no-owner --no-group
-  --del
   --out-format=%n
 );
 
@@ -33,7 +32,7 @@ my %changedTriggers = (
   "/home/user/.profiled/custom.ini" => "reload-profile"
 );
 
-sub overwriteFile($$);
+sub overwriteFile($$$);
 sub removeFile($);
 sub md5sum($);
 
@@ -62,7 +61,7 @@ sub main(@){
       if(defined $changedTriggers{$dest}){
         $old = md5sum $dest;
       }
-      overwriteFile "$DIR/$file", $dest;
+      overwriteFile "$DIR/$file", $dest, 1;
       if(defined $changedTriggers{$dest}){
         $new = md5sum $dest;
         if($old ne $new){
@@ -76,7 +75,7 @@ sub main(@){
   if($type =~ /^(bin|all)$/){
     print "\n ---handling bin files...\n";
     for my $file(@binFiles){
-      overwriteFile "$DIR/bin/$file", "$binTarget/$file";
+      overwriteFile "$DIR/bin/$file", "$binTarget/$file", 1;
     }
   }
 
@@ -96,17 +95,19 @@ sub main(@){
   system "chmod", "0440", "/etc/sudoers";
 }
 
-sub overwriteFile($$){
-  my ($src, $dest) = @_;
+sub overwriteFile($$$){
+  my ($src, $dest, $del) = @_;
   my $destDir = $dest;
   $destDir =~ s/\/[^\/]*$//;
 
   system "mkdir", "-p", $destDir;
   print "\n%%% $dest\n";
+  my @rsyncCmd = ("rsync", @rsyncOpts);
+  push @rsyncCmd, "--del" if $del;
   if(-d $src){
-    system 'rsync', @rsyncOpts, "$src/", "$dest";
+    system @rsyncCmd, "$src/", "$dest";
   }else{
-    system 'rsync', @rsyncOpts, "$src", "$dest";
+    system @rsyncCmd, "$src", "$dest";
   }
 
   if(defined $symlinksToReplace{$dest} and -l $dest){
